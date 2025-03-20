5052759531f40b8a971eb88cef0d90bc08bbeee2
Matteo Iacono <matteo@iacono.io>
2025-03-20 20:04:43 +0100

    jellyfin in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 curl -sfL https://get.k3s.io | sh -
+
+# Jellyfin
+mkdir -p /mnt/jellyfin/cache
+mkdir -p /mnt/jellyfin/config
+mkdir -p /mnt/jellyfin/media
+mkdir -p /mnt/jellyfin/vc
+
 kubectl create namespace jellyfin-ns
+
+find ./jellyfin/storage/ -maxdepth 1 -type f -name '*.yml' -exec kubectl apply -f {} \;
+find ./jellyfin/ -maxdepth 1 -type f -name '*.yml'  -exec kubectl apply -f {} \;
