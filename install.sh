aac94327005daa6d4fed73cb0c2454a857422057
Matteo Iacono <matteo@iacono.io>
2025-03-20 21:31:49 +0100

    Deluge added

--- install.sh
+++ install.sh
@@ -12,3 +12,12 @@ kubectl create namespace jellyfin-ns
 
 find ./jellyfin/storage/ -maxdepth 1 -type f -name '*.yml' -exec kubectl apply -f {} \;
 find ./jellyfin/ -maxdepth 1 -type f -name '*.yml'  -exec kubectl apply -f {} \;
+
+# Deluge
+mkdir -p /mnt/deluge/downloads
+mkdir -p /mnt/deluge/config
+
+kubectl create namespace deluge-ns
+
+find ./deluge/storage/ -maxdepth 1 -type f -name '*.yml' -exec kubectl apply -f {} \;
+find ./deluge/ -maxdepth 1 -type f -name '*.yml'  -exec kubectl apply -f {} \;
